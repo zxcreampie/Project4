9e75bb26d8dcdca2b4593624648b0cb8844d532b
zxcreampie <125575056+zxcreampie@users.noreply.github.com>
2023-02-27 18:17:47 +0300

    zxczxczxczxczxczxczxczxczxczxczxczxczxczxczxczxc

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -2,9 +2,7 @@
 #include <string>
 using namespace std;
 void rand_init(int* array, int size, int bound) {
-	for (int i = 0; i < size; i++) {
-		array[i] = rand() % bound;
-	}
+	
 }
 
 string convert(int* array, int size, int bound) {
